307f416b54339f01566b7e7c8508b00d46d246a7
isaach0011 <ihil17523@740s-pl-177.local>
2016-11-09 13:35:06 -0700

    Added Action listeners

--- src/gui/view/FirstFrame.java
+++ src/gui/view/FirstFrame.java
@@ -7,16 +7,20 @@ import gui.controller.GUIController;
 public class FirstFrame extends JFrame
 {
 	private GUIController baseController;
+	private FirstPanel appPanel;
 	
 	public FirstFrame(GUIController baseController)
 	{
 		super();
 		this.baseController = baseController;
+		this.appPanel = new FirstPanel(baseController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
+		this.setTitle("Boring App");
 		this.setSize(new Dimension(600, 400));
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -1,6 +1,71 @@
 package gui.view;
 
-public class FirstPanel
-{
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import gui.controller.GUIController;
 
+public class FirstPanel extends JPanel
+{
+	private GUIController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
+	private SpringLayout baseLayout;
+	
+	
+	public FirstPanel(GUIController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
+		this.colorButton = new JButton("Colors!");
+		this.randomButton = new JButton("Random Colors!");
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.DARK_GRAY);
+		this.add(colorButton);
+		this.add(randomButton);
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.NORTH, colorButton, 135, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, colorButton, 180, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, randomButton, 6, SpringLayout.SOUTH, colorButton);
+		baseLayout.putConstraint(SpringLayout.WEST, randomButton, 153, SpringLayout.WEST, this);
+	}
+	
+	private void setupListeners()
+	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				setBackground(Color.LIGHT_GRAY);
+			}
+		});
+		
+		randomButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				changeBackgroundColor();
+			}
+		});
+	}
+	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		this.setBackground(new Color(red, green, blue));
+	}
 }
